331f896a88ee5d9cd6200c3bf21b17d37332de02
LE2HE <leehe4352@gamil.com>
2022-08-04 00:13:47 +0900

    Implement #5 : 회원 가입 POST 구현
    회원 가입 구현
    - body-parser 등록
    - user 등록
    - app.post 구현
        - user 생성
        - user 저장

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// application/x-www-format-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true, useUnifiedTopology: true
@@ -12,6 +20,22 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/register', (req, res) => {
+  res.send('register...')
+})
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.send({ success: false, err })
+    return res.status(200).send({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
